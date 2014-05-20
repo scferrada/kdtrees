ebab6ca9e91e95e02923e5938446ab0671c0f9e9
scferrada <scferradaa@gmail.com>
2014-05-20 13:40:57 -0400

    Project structure

---
+++ src/KDTree.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sebastián
+ * Date: 20-05-14
+ * Time: 01:40 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KDTree {
+}

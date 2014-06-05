7d66db721cb40acc79c86465bb058517aa2a029a
scferrada <scferradaa@gmail.com>
2014-06-05 13:20:37 -0400

    Latex Structure

---
+++ src/KDMeanSecMemTree.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sebastián
+ * Date: 05-06-14
+ * Time: 12:04 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class KDMeanSecMemTree extends KDMeanTree {
+
+}
